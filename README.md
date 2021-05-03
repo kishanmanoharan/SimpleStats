afc7f7eb2202add251046c76133569eff14fcfdd
kishanmanoharan <45130704+kishanmanoharan@users.noreply.github.com>
2021-05-03 03:06:53 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # SimpleStats
 A simple system stats viewer
+
+- CPU
+    - Name
+    - Clock
+        - Core 1 [41]
+        - Core 2 [6]
+        - Core 3 [7]
+        - Core 4 [36]
+        - Core 5 [37]
+        - Core 6 [27]
+    - Load [15]
+    - Core Temp [46]
+    - Power [17]
+- GPU
+    - Name
+    - Clock [40]
+    - Load [2]
+    - Core Temp [13]
+    - Power [34]
+    - VRAM Load [25]
+    - VRAM Used/VRAM Total  [33],[32]
+- RAM
+    - Usage [14]
+    - RAM Used [16]
+    - RAM Free [39]
---
+++ main.js
@@ -0,0 +1,70 @@
+var wmi = require('node-wmi');
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+var win;
+function createWindow () {
+  win = new BrowserWindow({
+    width: 800,
+    height: 840,
+    frame: false, 
+    transparent: false,
+    webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false,
+      }
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+
+var sensors;
+var indexes = [41, 6, 7, 36, 37, 27, 15, 46, 17, 40, 2, 13, 34, 25, 33, 32, 14, 16, 39]
+function get_sensors() {
+    wmi.Query({
+        namespace: 'root/OpenHardwareMonitor',
+        class: 'sensor'
+    }, function(err, stats) {
+        // console.log(stats);
+        sensors = stats;
+    });
+    JSON.stringify(sensors);
+    // console.log('Sensors Updated');
+}
+
+function display_stats(sensors) {
+    var send = [];
+    for (i = 0; i < indexes.length; i++){
+        send.push(sensors[indexes[i]]);
+    }
+    win.webContents.send('sensor-data', send);
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+async function update(time) {
+    while (true) {
+        get_sensors();
+        await sleep(time);
+        display_stats(sensors);
+    }   
+}
+update(1000);
